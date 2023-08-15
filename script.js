ee2baee51f15993836db32c29fc81efd83fb7712
raven-blacck <1052548840@qq.com>
2023-08-15 17:37:42 +0200

    V1.1

--- script.js
+++ script.js
@@ -1,23 +1,22 @@
 
 document.addEventListener("DOMContentLoaded", function () {
-    var whiteBg = document.getElementById("white-background");
+
     var options = {
-        strings: ["Hello!","My name is Ziyang Chen (陈子扬)!","Welcome to my homepage!"],
-        typeSpeed: 20,
-        backSpeed: 8,
-        backDelay: 300,
-        startDelay: 100,
-        loop: false,
-        onComplete: function() {
-            // 设置透明度为0，开始CSS过渡效果
-            whiteBg.style.opacity = "0";
+        strings: ["Hello!<br>My name is Ziyang Chen (陈子扬)!<br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Welcome to my homepage!<br>"],
+        typeSpeed: 50,
+        backSpeed: 0,  // 设置为0确保文字不会被删除
+        backDelay: 1000,  // 因为我们不删除文字，所以设置为0
+        startDelay: 1000,  
+        loop: false,   // 不循环
+        showCursor: true,   // 保持光标可见
+        cursorChar: '|',    // 设置光标字符
+        bindInputFocusEvents: true,
+        onComplete: (self) => { // 当打字结束时触发
+            self.cursor.remove(); // 删除光标
         }
-        
     };
 
     var typed = new Typed("#typed-output" , options);
-    whiteBg.addEventListener("transitionend", function() {
-        whiteBg.style.display = "none";
-    });
 });
-    
+
+ 
